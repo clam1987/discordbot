5017a0a7d5e3dd5eb77abfe17478dd114b1baf88
Wilson Lam <lin-1987@live.com>
2020-03-11 17:48:14 -0700

    added comments to code

--- commands/how.js
+++ commands/how.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "?how",
-    description: "Ping!",
+    description: "Command to search stack overflow using the stack exchange api to grab results and returns results page. For now just returns Corrado telling you to google it.",
     execute(msg, args) {
         msg.reply('Corrado told me to tell you to Google it');
         msg.channel.send('Corrado told me to tell you to Google it');
--- discordBot.js
+++ discordBot.js
@@ -1,21 +1,27 @@
+// Discord Dependancies
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const dotenv = require("dotenv");
 dotenv.config();
 const token = process.env.botID;
+// creating new commands
 bot.commands = new Discord.Collection();
 const botCommands = require("./commands")
 
+// Mapping the commands
 Object.keys(botCommands).map(key => {
     bot.commands.set(botCommands[key].name, botCommands[key]);
 })
 
+// Bot to listen
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
-
+// Bot command to reply to messages
 bot.on("message", msg => {
+    // split content by white spaces
     const args = msg.content.split(/ +/);
+    // assuming the first args in our array is the command
     const command = args.shift().toLowerCase();
     console.info(`Called command: ${command}`);
 
